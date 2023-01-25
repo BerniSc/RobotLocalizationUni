1b4947db7d340b93c3af68d6099ea4b908800ea4
BerniSc <bernhard.schoeffel@gmail.com>
2023-01-25 14:06:09 +0100

    Optimized Code

--- control_window_params.cpp
+++ control_window_params.cpp
@@ -18,6 +18,7 @@ void parameterController::printCurrentConfig() {
 
 void parameterController::loadConfig(unsigned long configNr) {
     std::cout << "Loading Config " << configNr << " as current Config!" << std::endl;
+    
     std::vector<std::vector<std::string>> configData;
     readInData("param_config.pacf", configData);
     
@@ -28,7 +29,9 @@ void parameterController::loadConfig(unsigned long configNr) {
 
     std::vector<std::string> singleConfig = configData[configNr];
 
+    //Beginning with line one and ending with second to last as config has header and bottomline
     for(unsigned long i = 1; i < singleConfig.size() - 1; i++) {
+        //Seperating Data at '#' sign -> name left vs acutal value right
         std::vector<std::string> seperatedData = seperateString(singleConfig[i], "#");
         descriptors[i-1]->selectedValue = std::stoi(seperatedData[1]);
     }
@@ -53,9 +56,11 @@ cv::TrackbarCallback parameterDescription::getCallbackFunction() const {
 }
 
 int parameterDescription::getValue() const {
+    //if no selection return start Value
     if(parameterDescription::selectedValue == -1) {
         return parameterDescription::startValue;
     }
+    //return normalized Value
     return parameterDescription::selectedValue - parameterDescription::minValue;
 }
 
